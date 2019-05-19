3b0dc91f72b1f28f66de999ac485ad7361ea31f6
DanielRivasMD <danielrivasmd@gmail.com>
2019-05-19 20:51:38 +0200

    Update manuals

--- R/wrapper_functions.R
+++ R/wrapper_functions.R
@@ -2,6 +2,28 @@
 
 #----------------------------------------------------------------------------------------------------------------
 
+#' @title Error catcher
+#'
+#' @description Catches errors directly to stderr and continues execution.
+#' 'capture.output' is used to get the output of 'try' expression, whereas 'try' itself is used to evaluate whether or not 'expression' works
+#' @param f_expression Expression to be evaluated
+#' @return Error
+#' @importFrom utils capture.output
+#' @export
+#' @examples err_catch()
+
+err_catch <- function(f_expression=quote()){
+
+	err <- capture.output(try(f_out <- eval(f_expression)), type = "message")
+	if( length(err) > 0 ){
+		write(err, stderr())
+	}else{
+		return(f_out)
+	}
+}
+
+#----------------------------------------------------------------------------------------------------------------
+#
 # Deprecated
 # #' @title Bioconductor's biocLite() sourcing wrapper
 # #'
@@ -14,27 +36,5 @@
 # {
 #   source("https://bioconductor.org/biocLite.R")
 # }
-
-#----------------------------------------------------------------------------------------------------------------
-
-#' @title Error catcher
-#'
-#' @description Catches errors directly to stderr and continues execution.
-#' 'capture.output' is used to get the output of 'try' expression, whereas 'try' itself is used to evaluate whether or not 'expression' works
-#' @param f_expression Expression to be evaluated
-#' @return Error
-#' @importFrom utils capture.output
-#' @export
-#' @examples
-
-err_catch <- function(f_expression=quote()){
-
-	err <- capture.output(try(f_out <- eval(f_expression)), type = "message")
-	if( length(err) > 0 ){
-		write(err, stderr())
-	}else{
-		return(f_out)
-	}
-}
-
+#
 #----------------------------------------------------------------------------------------------------------------
