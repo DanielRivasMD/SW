e15891c397f44ba4032536f9782820a25b5843ac
DanielRivasMD <danielrivasmd@gmail.com>
2022-03-25 11:08:06 +0100

    add is installed function

---
+++ R/isInstalled.R
@@ -0,0 +1,16 @@
+
+#' @title Installed package finder
+#'
+#' @description Query whether package is installed
+#' @return Boolean
+#' @importFrom SystWrapper lsInstalled
+#' @examples isInstalled('SystWrapper')
+#' @export
+
+isInstalled <- function(pkg) {
+  if ( pkg %in% lsInstalled()[, 'Package'] ) {
+    print(paste0('Package is installed: ', pkg))
+  } else {
+    print(paste0('Package is not installed: ', pkg))
+  }
+}
