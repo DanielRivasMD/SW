3d9dc17ae216995964adfdc0bb58603c911095c8
DanielRivasMD <danielrivasmd@gmail.com>
2023-02-19 15:24:14 +0100

    explicit function usage

--- R/isInstalled.R
+++ R/isInstalled.R
@@ -8,7 +8,7 @@
 #' @export
 
 isInstalled <- function(pkg) {
-  if ( pkg %in% lsInstalled()[, 'Package'] ) {
+  if ( pkg %in% SystWrapper::lsInstalled()[, 'Package'] ) {
     print(paste0('Package is installed: ', pkg))
   } else {
     print(paste0('Package is not installed: ', pkg))
