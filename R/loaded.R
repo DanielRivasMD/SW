2a467b89497d26f484ca0bd41732f5217f2860a2
DanielRivasMD <danielrivasmd@gmail.com>
2023-08-27 15:09:50 -0600

    update loaded functions

--- R/loaded.R
+++ R/loaded.R
@@ -4,26 +4,23 @@
 #'
 #' \code{loaded} - Output is a character string of loaded packages.
 #'
-#' @param \ldots Optional package names.  Adding package names will check their
-#' individual load status.
-#' @param all logical.  If \code{TRUE} will show all packages
-#' including base install; \code{FALSE} will show all packages
-#' excluding base install packages that install when R loads.
-#' @param char Character vector containing packages to load.  If you are calling
-#' \code{loaded} from within a function (or just having difficulties calling it
-#' using a character vector input) then pass your character vector of packages
-#' to load to this parameter directly.
-#' @param character.only logical. If \code{TRUE} then \code{loaded} will only
-#' accept a single input which is a character vector containing the names of
-#' packages to load.
+#' @param all           logical. If \code{TRUE} will show all packages including base install; \code{FALSE} will show all packages excluding base install packages that install when R loads.
+#' @param char          Character vector containing packages to load. If you are calling \code{loaded} from within a function (or just having difficulties calling it  using a character vector input) then pass your character vector of packages to load to this parameter directly.
+#' @param char_only     logical. If \code{TRUE} then \code{loaded} will only accept a single input which is a character vector containing the names of packages to load.
+#' @param \ldots        Optional package names.  Adding package names will check their individual load status.
+#'
+#' @return              List of packages
+#' @examples            loaded()
+#'
+#' @rdname loaded
 #' @export
 
 ####################################################################################################
 
-loaded <- function(..., all = FALSE, char, character.only = FALSE) {
+loaded <- function(..., all = FALSE, char, char_only = FALSE) {
   if ( !missing(char) ) {
     packs <- char
-  } else if ( character.only ) {
+  } else if ( char_only ) {
     packs <- eval(match.call()[[2]])
   } else {
     packs <- tryCatch(as.character(match.call(expand.dots = FALSE)[[2]]), error=function(err) NA)
@@ -43,7 +40,7 @@ loaded <- function(..., all = FALSE, char, character.only = FALSE) {
   if ( !all && (length(packs) > 1 || !is.na(packs)) ) {
     packs <- as.character(packs)
 
-    #output <- packs %in% loaded
+    # output <- packs %in% loaded
     output <- packs %in% .packages() # make sure we can detect any loaded package
     names(output) <- packs
     return(output)
@@ -53,18 +50,23 @@ loaded <- function(..., all = FALSE, char, character.only = FALSE) {
 }
 
 ####################################################################################################
+####################################################################################################
 
 #' Check for Loaded Packages
 #'
-#' \code{isloaded} - Check if package(s) is loaded.
+#' \code{isLoaded} - Query whether package(s) is loaded.
+#'
+#' @return              Boolean
+#' @examples            isLoaded('stats')
 #'
 #' @rdname loaded
 #' @export
-isloaded <-
-  function(...) {
-    x <- substitute(...())
-    loaded(char = sapply(x, as.character))
 
-  }
+####################################################################################################
+
+isLoaded <- function(...) {
+  x <- substitute(...())
+  loaded(char = sapply(x, as.character))
+}
 
 ####################################################################################################
